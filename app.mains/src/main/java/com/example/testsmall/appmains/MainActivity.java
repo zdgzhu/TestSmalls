1afd891884a5c39aa00acf132257b1b78ba411a5
zhudonggang <zhudonggang>
2017-07-23 14:31:34 +0800

    点点滴滴

---
+++ app.mains/src/main/java/com/example/testsmall/appmains/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.testsmall.appmains;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //踩踩踩
+    }
+}
